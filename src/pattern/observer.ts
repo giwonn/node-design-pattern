13ab8fe1a8195a0073b630d3ed6c1e7518d64d6c
Allen / 임기원 <l4279625@gmail.com>
2022-03-26 03:20:42 +0900

    eventEmitter 관련 로직 수정

--- src/pattern/observer.ts
+++ src/pattern/observer.ts
@@ -1,28 +1,30 @@
 type EventName = string | symbol;
-type Listener = { once?: boolean; callback: Function };
+export type Listener = { once?: boolean; listener: Function };
+export type Listeners = Record<string, Listener[]>;
 interface IEventEmitter {
   on: (eventName: EventName, listener: Function) => EventEmitter; // 리스너 등록
   once: (eventName: EventName, listener: Function) => EventEmitter; // 리스너 한번만 실행되게 등록
   emit: (eventName: EventName, ...args: any) => boolean; // 등록되어 있는 리스너 호출
+  listeners: (eventName: EventName) => Array<Listener['listener']>;
 }
 
 // https://github.com/nodejs/node/blob/v18.x/lib/events.js 참고
-class EventEmitter implements IEventEmitter {
+export class EventEmitter implements IEventEmitter {
   private _events: Record<EventName, Listener[]> = {};
 
-  public on(eventName: EventName, callback: Function) {
+  public on(eventName: EventName, listener: Function) {
     this._events[eventName] ??= [];
-    this._events[eventName].push({ callback });
+    this._events[eventName].push({ listener });
     return this;
   }
 
-  public addListener(eventName: EventName, callback: Function) {
-    return this.on(eventName, callback);
+  public addListener(eventName: EventName, listener: Function) {
+    return this.on(eventName, listener);
   }
 
-  public once(eventName: EventName, callback: Function) {
+  public once(eventName: EventName, listener: Function) {
     this._events[eventName] ??= [];
-    this._events[eventName].push({ once: true, callback });
+    this._events[eventName].push({ once: true, listener });
     return this;
   }
 
@@ -35,8 +37,16 @@ class EventEmitter implements IEventEmitter {
     return true;
   }
 
+  public listeners(eventName: EventName) {
+    try {
+      return this._events[eventName].map(({ listener }) => listener);
+    } catch (error: any) {
+      throw new Error(error);
+    }
+  }
+
   private executeListeners(eventName: EventName, ...args: any[]) {
-    this._events[eventName].forEach(({ callback }) => callback(...args));
+    this._events[eventName].forEach(({ listener }) => listener(...args));
   }
 
   private filterOnceListner(eventName: EventName) {

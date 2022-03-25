bec93c4d6b9b6b63dbda0b4316afa8d2374b4822
Allen / 임기원 <l4279625@gmail.com>
2022-03-26 04:37:22 +0900

    EventEmitter: 테스트코드 리팩토링

--- tests/pattern/observer.spec.ts
+++ tests/pattern/observer.spec.ts
@@ -1,19 +1,22 @@
 import { EventEmitter } from '@/pattern/observer';
-import type { Listeners } from '@/pattern/observer';
 
 describe('Observer Pattern', () => {
-  const eventEmitter: EventEmitter = new EventEmitter();
-
   describe('Class - EventEmitter', () => {
+    const eventEmitter: EventEmitter = new EventEmitter();
     describe(`Method`, () => {
-      test('on() - 새로운 이벤트 등록 및 리스너 추가', () => {
-        eventEmitter.on('test', () => 'test1');
-        expect(eventEmitter.listeners('test')[0]()).toBe('test1'); // toBe() : 원시타입의 값, 객체의 참조값 비교
-      });
-      test('on() - 존재하는 이벤트에 리스너 추가', () => {
-        eventEmitter.on('test', () => 'test2');
-        const list = eventEmitter.listeners('test').map((listener) => listener());
-        expect(list).toEqual(['test1', 'test2']); // toEqual() : // toBe() : 래퍼 타입, 객체 내부의 값 비교
+      describe('on()', () => {
+        test('새로운 이벤트 등록 및 리스너 추가', () => {
+          eventEmitter.on('test', () => 'test1');
+          const sample = 'test1';
+          expect(eventEmitter.listeners('test')[0]()).toBe(sample); // toBe() : 원시타입의 값, 객체의 참조값 비교
+        });
+
+        test('존재하는 이벤트에 리스너 추가', () => {
+          eventEmitter.on('test', () => 'test2');
+          const ReturnValuesByValues = eventEmitter.listeners('test').map((listener) => listener());
+          const sample = ['test1', 'test2'];
+          expect(ReturnValuesByValues).toEqual(sample); // toEqual() : 래퍼 타입, 객체 내부의 값 비교
+        });
       });
     });
   });

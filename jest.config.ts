a5cb3d9ba32c7381872bef8b274d81d1b7851cff
Allen / 임기원 <l4279625@gmail.com>
2022-03-26 04:26:52 +0900

    jest 세팅 변경

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,15 @@
-module.exports = {
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  verbose: true,
   preset: 'ts-jest', //trypeScript파일은 ts-jest에서 CommonJS구문으로 변환
   transform: {
-    '^.+\\.ts': 'babel-jest',
+    '^.+\\.ts': 'ts-jest',
   },
   testEnvironment: 'node', //테스트 환경
   testMatch: ['<rootDir>/tests/**/*.spec.ts'], //테스트 파일 위치
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
 };
+export default config;

651ced910ce681b6aefe82fcb1b20b25a79e3d7c
Allen / 임기원 <l4279625@gmail.com>
2022-03-26 03:56:28 +0900

    앞으로 추가할 메서드들 인터페이스에 추가

--- src/pattern/observer.ts
+++ src/pattern/observer.ts
@@ -6,12 +6,15 @@ interface IEventEmitter {
   on: (eventName: EventName, callback: Function) => EventEmitter; // 리스너 등록
   once: (eventName: EventName, callback: Function) => EventEmitter; // 리스너 한번만 실행되게 등록
   emit: (eventName: EventName, ...args: any) => boolean; // 등록되어 있는 리스너 호출
+  eventNames: () => EventName[];
+  getMaxListeners: () => number;
   listeners: (eventName: EventName) => Array<Listener['listener']>;
 }
 
 // https://github.com/nodejs/node/blob/v18.x/lib/events.js 참고
 // TODO : 이벤트명이 'newListener'인 이벤트 => 모든 이벤트는 등록될 때 newListener 이벤트를 호출한다. 즉, 이걸 이용해서 이벤트에 추가적인 행동을 취할 수 있게 해줌
 // TODO : 이벤트명이 'removeListner'인 이벤트 => 어떤 이벤트든 제거된 후에 removeListner가 호출됨. 마찬가지로 추가적인 행동 가능
+// 인터페이스에 구현안된 함수들 다 구현하기
 
 export class EventEmitter implements IEventEmitter {
   private _events: Record<EventName, Listener[]> = {};
@@ -23,24 +26,13 @@ export class EventEmitter implements IEventEmitter {
   }
 
   public on(eventName: EventName, callback: Function) {
-    this._events[eventName] ??= [];
-    this._events[eventName].push({ listener: callback });
-    return this;
+    return this.addListener(eventName, callback);
   }
 
   public once(eventName: EventName, callback: Function) {
     this._events[eventName] ??= [];
     this._events[eventName].push({ once: true, listener: callback });
     return this;
-  }
-
-  public emit(eventName: EventName, ...args: any) {
-    if (this._events[eventName] === undefined) return false;
-
-    this.executeListeners(eventName, args);
-    this._events[eventName] = this.filterOnceListner(eventName);
-
-    return true;
   }
 
   public listeners(eventName: EventName) {
@@ -51,6 +43,14 @@ export class EventEmitter implements IEventEmitter {
     }
   }
 
+  public emit(eventName: EventName, ...args: any) {
+    if (this._events[eventName] === undefined) return false;
+
+    this.executeListeners(eventName, args);
+    this._events[eventName] = this.filterOnceListner(eventName);
+
+    return true;
+  }
   private executeListeners(eventName: EventName, ...args: any[]) {
     this._events[eventName].forEach(({ listener }) => listener(...args));
   }
